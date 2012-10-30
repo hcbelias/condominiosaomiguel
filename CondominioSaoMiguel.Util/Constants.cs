559abd8557756aec74f438d4fab71727f4c6224a
henriqueelias <henriqueelias@de2369d9-6d6e-4cb1-9dc9-00429f6a12b2>
2012-10-30 01:02:10 +0000

    Correçãoi do path

---
+++ CondominioSaoMiguel.Util/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CondominioSaoMiguel.Util
+{
+    public static class Constants
+    {
+        public static class Configuration
+        {
+            public static String RESOURCES_FOLDER_PATH { get { return "IMAGES_DIRECTORY_PATH"; } }
+        }
+    }
+}
---
+++ CondominioSaoMiguel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CondominioSaoMiguel.MvcApplication" Language="C#" %>
