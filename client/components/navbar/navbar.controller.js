ccab26a96942916e4917b8c482e6f30b57199da7
henrique elias <hcbe2004@gmail.com>
2015-09-26 11:11:11 -0300

    basic filter for navbars

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,14 +1,28 @@
 'use strict';
 
 angular.module('condominiosmApp')
-  .controller('NavbarCtrl', function ($scope, Auth) {
+  .controller('NavbarCtrl', function ($scope, Auth, ptBR, $state) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': ptBR.HOME,
       'state': 'main'
+    },
+    {
+      'title': ptBR.STRUCTURE,
+      'state': 'structure'
+    },
+    {
+      'title': ptBR.LOCALIZATION,
+      'state': 'localization'
+    },
+    {
+      'title': ptBR.APARTMENTS,
+      'state': 'apartments'
     }];
 
     $scope.isCollapsed = true;
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+    
+    $scope.Title = $scope.menu[0].title; //$state.current.name
   });
