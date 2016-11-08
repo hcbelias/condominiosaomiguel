b4e3c46d087fca0b9886ba4ba5a8d431980798e2
henrique elias <hcbe2004@gmail.com>
2016-11-07 22:14:02 -0200

    dcsdvs

--- client/app/app.js
+++ client/app/app.js
@@ -9,54 +9,35 @@ angular.module('condominiosmApp', [
   'pascalprecht.translate'
 ])
   .config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider, $translateProvider, ptBR) {
-	  //I18N
+	    //I18N
       $translateProvider.translations('pt-BR', ptBR);
       $translateProvider.preferredLanguage('pt-BR');
       $translateProvider.useSanitizeValueStrategy('escape');
-    $urlRouterProvider
-      .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-    $httpProvider.interceptors.push('authInterceptor');
-  })
-
-  .factory('authInterceptor', function($rootScope, $q, $cookies, $injector) {
-    var state;
-    return {
-      // Add authorization token to headers
-      request: function(config) {
-        config.headers = config.headers || {};
-        if ($cookies.get('token')) {
-          config.headers.Authorization = 'Bearer ' + $cookies.get('token');
-        }
-        return config;
-      },
-
-      // Intercept 401s and redirect you to login
-      responseError: function(response) {
-        if (response.status === 401) {
-          (state || (state = $injector.get('$state'))).go('login');
-          // remove any stale tokens
-          $cookies.remove('token');
-          return $q.reject(response);
-        }
-        else {
-          return $q.reject(response);
-        }
-      }
-    };
-  })
-
-  .run(function($rootScope, $state, Auth) {
-    // Redirect to login if route requires auth and the user is not logged in
-    $rootScope.$on('$stateChangeStart', function(event, next) {
-      if (next.authenticate) {
-        Auth.isLoggedIn(function(loggedIn) {
-          if (!loggedIn) {
-            event.preventDefault();
-            $state.go('login');
-          }
+      //Route
+      $stateProvider    
+        .state('main', {
+            url: '#/',
+            templateUrl: 'app/main/main.html',
+            controlleR: 'MainCtrl'
+        })
+        .state('structure', {
+            url: '#/structure',
+            templateUrl: 'app/structure/structure.html',
+            controlleR: 'StructureCtrl'
+        })
+        .state('location', {
+            url: '#/location',
+            templateUrl: 'app/location/location.html',
+            controlleR: 'LocationCtrl'
+        })
+        .state('contact', {
+            url: '#/contact',
+            templateUrl: 'app/contact/contact.html',
+            controlleR: 'ContactCtrl'
         });
-      }
-    });
+
+      $urlRouterProvider.otherwise('/');
+
+      $locationProvider.html5Mode(true);
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('condominiosmApp')
-  .controller('MainCtrl', function() {
+  .controller('MainCtrl', function($scope, Email) {
     
   });
