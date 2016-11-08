82a8229925656234bdd76f7255097c9ff4e1f97d
henrique elias <hcbe2004@gmail.com>
2016-11-08 21:42:34 -0200

    changes onm struture

--- client/components/i18n/pt-BR.js
+++ client/components/i18n/pt-BR.js
@@ -3,6 +3,7 @@
 angular.module('condominiosmApp').
 	constant('ptBR', {
 		APARTMENTS: 'Apartamentos',
+		ALT: 'Condomínio São Miguel - Sua moradia, com comforto e segurança, em Alfenas - Alugue nossos apartamentos',
 		COMFORT: 'Comforto',
 		CONDOMINIO_SAO_MIGUEL: 'Condomínio São Miguel',
 		CONTACT: 'Contato',
